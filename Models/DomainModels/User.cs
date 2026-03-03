0633bc13f1e87e73f975bf0b723bd7ef49239051
muzamil reshi <muzamilreshi0008@gmail.com>
2026-03-03 13:15:25 +0530

    Added CORS fix and backend update

--- Models/DomainModels/User.cs
+++ Models/DomainModels/User.cs
@@ -10,12 +10,12 @@ public class User
 {
   [Key]
     public  Guid UserId {get;set;} = Guid.NewGuid();
-    public required string  Username {get ;set;}
+    // public required string  Username {get ;set;}
     public required string  Email {get ;set;}
     public required string  Password {get ;set;}
-    public string?  ProfilePicUrl {get ;set;}
-    public string? Phone {get; set ; }
+    // public string?  ProfilePicUrl {get ;set;}
+    // public string? Phone {get; set ; }
  
-     public Role Role {get; set;} = Role.User;
+    //  public Role Role {get; set;} = Role.User;
 }
 
