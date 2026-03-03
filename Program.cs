8abd675bb8256e7715c205222c71c97f5506860c
muzamil reshi <muzamilreshi0008@gmail.com>
2026-03-03 13:07:14 +0530

    Added CORS fix and backend update

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ using MyWebApplication.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Controllers
+builder.Services.AddControllers();
 
+// CORS Policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
@@ -15,50 +18,22 @@ builder.Services.AddCors(options =>
         });
 });
 
+// Database
+builder.Services.AddDbContext<SqlDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("kashmirTours")));
 
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
- 
-builder.Services.AddControllersWithViews();
-builder.Services.AddSession();
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddAuthentication(); 
-
-// Add services to the container.
-// builder.Services.AddSingleton<ITokenService , TokenService>();
-// builder.Services.AddSingleton<IClouldinaryService,CloudinaryService>();
-builder.Services.AddControllersWithViews();
-// builder.Services.AddScoped<IMailService, EmailService>();
-builder.Services.AddDbContext<SqlDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("kashmirTours")));
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-  
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-
-app.UseExceptionHandler("/Error");
-app.UseStaticFiles();
+// Middleware Order Important
 app.UseCors("AllowFrontend");
-app.UseSession();
+
 app.UseHttpsRedirection();
-app.UseStaticFiles();   // use static files present in wwwwroot 
+
 app.UseRouting();
-app.UseAuthentication();
+
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers(); // IMPORTANT
 
 app.Run();
-
-
- 
